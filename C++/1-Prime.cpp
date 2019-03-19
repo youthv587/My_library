f2ddac8c0cc4d0b6eb5849b68a9f7c981f176252
youthv587 <43020597+youthv587@users.noreply.github.com>
2019-03-19 21:56:36 +0800

    Update 1-Prime.cpp

--- C++/1-Prime.cpp
+++ C++/1-Prime.cpp
@@ -5,19 +5,21 @@ int main(){
   bool isPrime(int a);
   int N,i=0,num=0;
 
+//无限判断
 while(1){
 	cin>>N;
 	if(isPrime(N))cout<<"yes"<<endl;
-	else break;
-}
+	else cout<<"no"<<endl;
+	}
 
 }
 
+//函数判断体
  bool isPrime(int a){
    if(a==1)return false;
    else if(a==2)return true;
    else{
-    for(int i=3;i*i<=a;i+=2){
+    for(int i=2;i*i<=a;i++){
       if(a%i==0)return false;
       }
       return true;
